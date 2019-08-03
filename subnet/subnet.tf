0359351e851fa72b2ddc1120b99433c8adaae1aa
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-08-03 08:11:51 +0800

    Lifecycle management

--- subnet/subnet.tf
+++ subnet/subnet.tf
@@ -4,6 +4,10 @@
 resource "azurerm_subnet" "v_subnet" {
   count                   = length(local.subnet_list) 
 
+    lifecycle {
+        ignore_changes = [network_security_group_id]
+    }
+
   name                    = var.subnets[element(local.subnet_list, count.index)].name
   resource_group_name     = var.resource_group
   virtual_network_name    = var.virtual_network_name
