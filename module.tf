15aa83b23b021be848d5c204c29d56d885b3db14
Laurent <laurent.lesle@microsoft.com>
2019-09-24 20:06:45 +0800

    removing regions, updating tags, fixing nsg vnet association causing destroy to fail

--- module.tf
+++ module.tf
@@ -1,43 +1,51 @@
 // Creates the networks virtual network, the subnets and associated NSG, with a special section for AzureFirewallSubnet
 
 resource "azurerm_virtual_network" "vnet" {
-  name                  = "${var.prefix}${var.networking_object["region1"].vnet.name}"
+  name                  = "${var.prefix}${var.networking_object.vnet.name}"
   location              = var.location
   resource_group_name   = var.virtual_network_rg
-  address_space         = var.networking_object["region1"].vnet.address_space
-  dns_servers           = var.networking_object["region1"].vnet.dns
-  tags                  = var.tags
+  address_space         = var.networking_object.vnet.address_space
+  dns_servers           = var.networking_object.vnet.dns
+  tags                  = local.tags
 }
 
-module "special_subnets_region1" {
+module "special_subnets" {
   source                = "./subnet"
 
   resource_group        = var.virtual_network_rg
   virtual_network_name  = azurerm_virtual_network.vnet.name
-  subnets               = var.networking_object["region1"].specialsubnets
-  tags                  = var.tags
+  subnets               = var.networking_object.specialsubnets
+  tags                  = local.tags
   location              = var.location
 }
 
-module "subnets_region1" {
+module "subnets" {
   source                = "./subnet"
 
   resource_group        = var.virtual_network_rg
   virtual_network_name  = azurerm_virtual_network.vnet.name
-  subnets               = var.networking_object["region1"].subnets
-  tags                  = var.tags
+  subnets               = var.networking_object.subnets
+  tags                  = local.tags
   location              = var.location
 }
 
-module "nsg_region1" {
+module "nsg" {
   source                    = "./nsg"
 
   resource_group            = var.virtual_network_rg
   virtual_network_name      = azurerm_virtual_network.vnet.name
-  subnets                   = var.networking_object["region1"].subnets
-  tags                      = var.tags
+  subnets                   = var.networking_object.subnets
+  tags                      = local.tags
   location                  = var.location
   log_analytics_workspace   = var.log_analytics_workspace
   diagnostics_map           = var.diagnostics_map
 }
 
+
+
+resource "azurerm_subnet_network_security_group_association" "nsg_vnet_association" {
+  for_each                  = module.subnets.subnet_ids_map
+
+  subnet_id                 = module.subnets.subnet_ids_map[each.key].id
+  network_security_group_id = module.nsg.nsg_obj[each.key].id
+}
--- nsg_stitching.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet_network_security_group_association" "nsg_vnet_association_region1" {
-  for_each                  = var.networking_object["region1"].subnets
-
-  subnet_id                 = module.subnets_region1.subnet_ids_map[each.key].id
-  network_security_group_id = module.nsg_region1.nsg_obj[each.key].id
-}
